eb0fe96d4a9f9de382f197df9d19218dd06a3d5f
YASHWANTH J <jy2723@srmist.edu.in>
2024-03-08 10:48:03 +0530

    [yashwanth] added call back

--- sample.js
+++ sample.js
@@ -1,21 +1,24 @@
+// const student = {
+//     fullname : "yash",
+//     age : 20,
+//     cgpa : 9.2
 
-function vowels(str){
-    let count = 0
-    for(let i=0;i<str.length;i++){
-        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
-        count += 1;
-        }   
-    }
-    return count
-}
-// arrow function
-const count_vowels = (str) => {
-    let count = 0
-    for(let i=0;i<str.length;i++){
-        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
-        count += 1;
-        }   
-    }
-    return count
+// };
+// console.log(student.age)
+
+// arr = [200, 300, 600 , 5000]
+// for( let i=0;i<arr.length;i++){
+//     offer = arr[i]*(1/10);
+//     arr[i] = arr[i] - offer;
+//     console.log(arr[i]);
+// }
+
+
+
+// call back
+nums = [1,5,6,8,7]
+let calsquare = (num => {
+    console.log(num * num);
     
-}
+});
+nums.forEach(calsquare)
