e8ea277ea420d6bdd7739ae417b3486948678b88
YASHWANTH J <jy2723@srmist.edu.in>
2024-03-08 11:11:06 +0530

    [yashwanth] added hoisting

--- sample.js
+++ sample.js
@@ -1,49 +1,10 @@
-// const student = {
-//     fullname : "yash",
-//     age : 20,
-//     cgpa : 9.2
-
-// };
-// console.log(student.age)
-
-// arr = [200, 300, 600 , 5000]
-// for( let i=0;i<arr.length;i++){
-//     offer = arr[i]*(1/10);
-//     arr[i] = arr[i] - offer;
-//     console.log(arr[i]);
-// }
-
-
-
-
-// map method
-nums = [1,5,6,8,7]
-let calsquare = (num => {
-    console.log(num * num);
-    
-});
-let new_arr = nums.map((val) =>{
-    return val * val;
-});
-console.log(new_arr)
-
-// filter method
-nums = [1,5,6,8,7]
-let even_arr = nums.filter((val) =>{
-    return val %2 ==0 ;
-});
-console.log(even_arr)
-let n =prompt("enter number of elements:")
-
-arr=[];
-for(let i=1;i<=n;i++){
-    arr[i-1] = i
-}
-console.log(arr)
-// reduce method
-let sum = arr.reduce((prev, curr) => {
-    return prev+curr
-}
-)
-console.log(sum)
-
+// Function hoisting
+function greet() {
+    console.log('Hello, world!');
+  }
+  
+  greet(); // This will print "Hello, world!" even though the function is declared after it is called.
+  
+  // Variable hoisting
+  console.log(name); // This will print "undefined" because the variable is not initialized yet.
+  var name = 'John Doe';
