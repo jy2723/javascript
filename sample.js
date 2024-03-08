3ca186e91a08708dd479ec53442d01a64da8b040
YASHWANTH J <jy2723@srmist.edu.in>
2024-03-08 12:28:04 +0530

    [yashwanth] added callback hell

--- sample.js
+++ sample.js
@@ -1,10 +1,16 @@
-// Function hoisting
-function greet() {
-    console.log('Hello, world!');
-  }
-  
-  greet(); // This will print "Hello, world!" even though the function is declared after it is called.
-  
-  // Variable hoisting
-  console.log(name); // This will print "undefined" because the variable is not initialized yet.
-  var name = 'John Doe';
+function getdata(dataid, getnextdata){
+    setTimeout(() => {
+        console.log("data",dataid)
+        if (getnextdata){
+            getnextdata()
+        }
+    }, 3000);
+}
+
+getdata(1, ()=>{
+    getdata(2, ()=>{
+        getdata(3,()=>{
+            getdata(4)
+        })
+    })
+})
